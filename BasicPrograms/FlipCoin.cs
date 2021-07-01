dcf1577bbee1389cd9d3412666777138af1fbb2e
VivekG11 <vivekvk2903@gmail.com>
2021-07-01 16:16:49 +0530

    [Basic Programs]

---
+++ BasicPrograms/FlipCoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class FlipCoin
+    {
+        public static void CoinFcaePercentage()
+        {
+            int num,head = 0, tail = 0;
+            Console.WriteLine("Enter a number to find number of times to flip a coin : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            for (int i =0; i < num; i++)
+            {
+                double randomValue = random.NextDouble();
+                if (randomValue < 0.5)
+                {
+                    tail++;
+                }
+                else 
+                {
+                    head++;
+                }
+            }
+            Console.WriteLine("Total Number of tails = "+tail);
+            Console.WriteLine("Total Number of heads = " + head);
+            double tailPercentage = (double) tail / num * 100;
+            double headPercentage = 100 - tailPercentage;
+            Console.WriteLine("The percenatge of tails is : "+tailPercentage);
+            Console.WriteLine("The percenatge of heads is : " + headPercentage);
+        }
+    }
+}
---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FlipCoin.CoinFcaePercentage();
+        }
+    }
+}
